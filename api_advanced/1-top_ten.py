145cebd3ea8dc2d55b4b325852b6ae12b9a0f311
Keane <g.keane@alustudent.com>
2025-03-19 15:19:13 -0700

    initial commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,15 +4,28 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Chrome/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    # print(response)
-    print(response.text[189097:])
-    if response.status_code != 200:
-        print(None)
-    else:
-        posts = response.json().get('data').get('children')
-        for post in posts:
-            print(post['data']['title'])
+    """Prints the titles of the first 10 hot posts listed in a subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    
+    # More realistic User-Agent string to avoid being blocked
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check for response status code
+        if response.status_code != 200:
+            print(None)  # Invalid subreddit or other issues
+        else:
+            # If the status code is 200, process the response
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+
+            # Print the titles of the first 10 posts
+            for post in posts:
+                print(post['data']['title'])
+
+    except requests.exceptions.RequestException:
+        print(None)  # If a network or other exception occurs
